81c45c680e489f2437291f452c20532dd20a764f
Jackson Theel <62031868+jacksontheel@users.noreply.github.com>
2025-10-22 18:51:15 -0600

    57 add formatting for no match messages (#96)

--- world/entities/event.go
+++ world/entities/event.go
@@ -1,5 +1,13 @@
 package entities
 
+import (
+	"fmt"
+	"strconv"
+
+	"example.com/mud/models"
+	"example.com/mud/utils"
+)
+
 // allows us to use the bus without tightly coupling a
 // specific publisher to our world model
 type Publisher interface {
@@ -21,4 +29,65 @@ type Event struct {
 type Rule struct {
 	When []Condition
 	Then []Action
+}
+
+func FormatEventMessage(message string, ev *Event) (string, error) {
+	eventMap := make(map[string]string, 4)
+
+	eventMap[EventRoleMessage.String()] = ev.Message
+
+	if ev.Source != nil {
+		role := EventRoleSource.String()
+		eventMap[role] = ev.Source.Name
+
+		for f, v := range ev.Source.Fields {
+			switch v.K {
+			case models.KindBool:
+				eventMap[fmt.Sprintf("%s.%s", role, f)] = strconv.FormatBool(v.B)
+			case models.KindInt:
+				eventMap[fmt.Sprintf("%s.%s", role, f)] = strconv.FormatInt(int64(v.I), 10)
+			case models.KindString:
+				eventMap[fmt.Sprintf("%s.%s", role, f)] = v.S
+			}
+		}
+	}
+
+	if ev.Instrument != nil {
+		role := EventRoleInstrument.String()
+		eventMap[role] = ev.Instrument.Name
+
+		for f, v := range ev.Instrument.Fields {
+			switch v.K {
+			case models.KindBool:
+				eventMap[fmt.Sprintf("%s.%s", role, f)] = strconv.FormatBool(v.B)
+			case models.KindInt:
+				eventMap[fmt.Sprintf("%s.%s", role, f)] = strconv.FormatInt(int64(v.I), 10)
+			case models.KindString:
+				eventMap[fmt.Sprintf("%s.%s", role, f)] = v.S
+			}
+		}
+	}
+
+	if ev.Target != nil {
+		role := EventRoleTarget.String()
+		eventMap[role] = ev.Target.Name
+
+		for f, v := range ev.Target.Fields {
+			switch v.K {
+			case models.KindBool:
+				eventMap[fmt.Sprintf("%s.%s", role, f)] = strconv.FormatBool(v.B)
+			case models.KindInt:
+				eventMap[fmt.Sprintf("%s.%s", role, f)] = strconv.FormatInt(int64(v.I), 10)
+			case models.KindString:
+				eventMap[fmt.Sprintf("%s.%s", role, f)] = v.S
+			}
+		}
+	}
+
+	message, err := utils.FormatText(message, eventMap)
+	if err != nil {
+		return "", fmt.Errorf("format event message: %w", err)
+	}
+
+	return message, nil
 }
